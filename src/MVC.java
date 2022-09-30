4ed005138144952c3edf7e7d07ecf861dd6db499
tyckobra <77719515+tyckobra@users.noreply.github.com>
2022-09-30 13:13:25 +0200

    Initial commit

---
+++ src/MVC.java
@@ -0,0 +1,15 @@
+
+
+import java.io.File;
+import java.util.Scanner;
+
+public class MVC {
+    public static void main(String[] args) {
+
+        File Key = new File("Key.txt");
+
+        System.out.println(Key);
+
+        
+    }
+}
