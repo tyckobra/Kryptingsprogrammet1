d9f18e83bea8857d3354b567797ed257783a0587
tyckobra <77719515+tyckobra@users.noreply.github.com>
2022-10-07 13:40:46 +0200

    22-10-07

--- src/GUI.java
+++ src/GUI.java
@@ -1,2 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class GUI {
+        public static void main(String args[]){
+            JFrame frame = new JFrame("GUI");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(800,200);
+
+            JLabel message = new JLabel("Enter Message");
+            JTextField messagefield = new JTextField(10);
+
+            JLabel key = new JLabel("Enter Key");
+            JTextField keyfield = new JTextField(10);
+
+            JLabel resultat = new JLabel("Resultat");
+            JTextField resultatfield = new JTextField(10);
+
+            JButton crypt = new JButton("CRYPT");
+            frame.getContentPane().add(crypt);
+
+            JPanel panelmessage = new JPanel();
+            JPanel panelkey = new JPanel();
+            JPanel panelresultat = new JPanel();
+
+            panelmessage.add(message);
+            panelmessage.add(messagefield);
+            panelkey.add(key);
+            panelkey.add(keyfield);
+            panelresultat.add(resultat);
+            panelresultat.add(resultatfield);
+
+            frame.getContentPane().add(BorderLayout.WEST, panelmessage);
+            frame.getContentPane().add(BorderLayout.CENTER, panelkey);
+            frame.getContentPane().add(BorderLayout.EAST, panelresultat);
+
+            frame.setVisible(true);
+
+    }
 }
--- src/Model.java
+++ src/Model.java
@@ -4,27 +4,40 @@ public class Model {
         String k = "Pipa";
 
         Model cryptModel = new Model();
+        //System.out.println((char)cryptModel.crypt(m,k));
         System.out.println(cryptModel.cryptString(m,k));
+        System.out.println(cryptModel.binärString(m,k));
     }
+
     public String cryptString(String message, String key) {
         String resultat = "";
+
         while (key.length() < message.length()) {
             key = expandKey(key);
         }
         for (int i = 0 ; i < message.length() ; i++) {
-            resultat += (char)crypt(message.chatAt(i), key.charAt(i));
+            resultat += (char)crypt(message.charAt(i), key.charAt(i));
         }
         return resultat;
     }
 
-    public int crypt(String m, String k) {
+    public String binärString(String message, String key) {
+        String binärt = "";
+
+        while (key.length() < message.length()) {
+            key = expandKey(key);
+        }
+        for (int i = 0 ; i < message.length() ; i++) {
+            binärt += crypt(message.charAt(i), key.charAt(i));
+        }
+        return binärt;
+    }
+
+    public int crypt(int m, int k) {
         return m^k;
     }
 
-    public int expandKey{
-        if (message.length > key.length) {
-            key+key;
-        }
-
+    public String expandKey(String s){
+            return s+s;
     }
 }
